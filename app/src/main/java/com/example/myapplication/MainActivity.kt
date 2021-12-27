09f390c4b07c04041b6d178078873b0e7e3b407d
LeeWH <ddang000@gmail.com>
2021-12-27 16:01:49 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test = "branch1"
+        val test = "branch1 add"
     }
 }
